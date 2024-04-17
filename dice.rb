93933db8706295e414e6e3fed4f7d64aa93873e2
drdpatel <165212603+drdpatel@users.noreply.github.com>
2024-04-17 14:43:07 +0000

    get do worked

--- dice.rb
+++ dice.rb
@@ -1,6 +1,13 @@
 #/ dice.rb
 
 require "sinatra"
-get("/") do
-  return "Hello, world!"
+
+get "/" do
+  "Hello World"
 end
+
+get("/zebra") do
+ "We must add a route for each path we want to support"
+end
+
+# https://psychic-computing-machine-7v7xwgvxw5wj26gq.github.dev/zebra
